39361eee0c4be331ffdfd623678606049810857b
yuanyc <yuanyc@mapbar.com>
2017-04-28 17:34:27 +0800

    chg 创建bitmap处理工具类

--- app/src/main/java/annotation/ViewInjectUtils.java
+++ app/src/main/java/annotation/ViewInjectUtils.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Objects;
 
 /**
  * Author ：yuanyc
---
+++ app/src/main/java/bitmapUtils/MyBitmapUtils.java
@@ -0,0 +1,12 @@
+package bitmapUtils;
+
+/**
+ * Author ：yuanyc
+ * Time ：2017/4/28
+ * Description ：bitmap处理工具类
+ */
+
+public class MyBitmapUtils {
+    //写此类的时候注意提供静态方法，注意注释，注意代码的简洁
+
+}
